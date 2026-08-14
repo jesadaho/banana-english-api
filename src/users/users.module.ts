25c0f7beb657131c979dffed709d095c3e0dc889
Jesada <jesada@Jesadas-MacBook-Pro.local>
2026-08-14 21:09:23 +0700

    make it better

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EconomyModule } from '../economy/economy.module';
 import { ActivityService } from './activity.service';
 import { AnonymousUserGuard } from './anonymous-user.guard';
+import { UserNotificationsService } from './user-notifications.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
